8e2302f4ada1f6f751bab28508d01a9488853507
marjanak <marjana1816@gmail.com>
2025-01-28 12:02:20 -0800

    Initial Commit

---
+++ FoodAtHomeFE/FoodAtHomeFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAtHomeFEApp.swift
+//  FoodAtHomeFE
+//
+//  Created by New Student on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodAtHomeFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
